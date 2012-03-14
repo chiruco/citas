8070f00a88deeed14692bb04f0a37e0dd786abfe
victor <vmolina@tigo.com.gt>
2012-03-13 19:33:08 -0600

    mi primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '39c76d6df7539bd447bb94d0349ab42abfcb59e9d6324df070fd8c1fd233549dd51da0561d16615277e858f4fb8e6e4229ddefd5900f1ea835938c53c314009c'
